6bbd6f67d9bbc5cdfc56acc6368f6a569eff1d66
cesdouglas <cesdouglas@hotmail.com>
2015-09-24 22:37:15 -0300

    Primeiro Commit

---
+++ src/br/com/fiap/teste/TesteConexao.java
@@ -0,0 +1,9 @@
+package br.com.fiap.teste;
+
+import br.com.fiap.conexao.ConexaoFactory;
+
+public class TesteConexao {
+	public static void main(String[] args) {
+		ConexaoFactory c = new ConexaoFactory();
+	}
+}
