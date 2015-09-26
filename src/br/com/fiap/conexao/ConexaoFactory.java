f14bdf1cbed7665a868ab2dc4d1350bf41e60ec4
brunodamo <damobruno@gmail.com>
2015-09-26 19:53:44 -0300

    blalbla

--- src/br/com/fiap/conexao/ConexaoFactory.java
+++ src/br/com/fiap/conexao/ConexaoFactory.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConexaoFactory {
+	//DriverManager
 	public Connection getConnection() throws Exception{
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		return DriverManager.getConnection("jdbc:oracle:thin:/:@oracle.fiap.com.br:1521:ORCL", "OPS$RM73872", "290992");
