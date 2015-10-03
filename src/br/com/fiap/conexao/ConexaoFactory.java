52e9d83fdbd408331aea8856866053cf0d4f9217
Douglas <cesdouglas@hotmail.com>
2015-10-03 01:31:48 -0300

    "teste ignore"

--- src/br/com/fiap/conexao/ConexaoFactory.java
+++ src/br/com/fiap/conexao/ConexaoFactory.java
@@ -9,5 +9,6 @@ public class ConexaoFactory {
 	public Connection getConnection() throws Exception{
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		return DriverManager.getConnection("jdbc:oracle:thin:/:@oracle.fiap.com.br:1521:ORCL", "OPS$RM73872", "290992");
+		
 	}
 }
